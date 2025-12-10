ed5ea3f76aa3f2fd6edab95d5598f5cf8325b4ac
IdanPRoss <Rossinidan01@gmail.com>
2025-12-10 15:19:03 +0200

    New Project Design Pattern Created

---
+++ TelHai.CS.CsharpCourse.DesignPatterns/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TelHai.CS.CsharpCourse.DesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
